c4f0c33061f33741ad506cf9e648000f8c406e02
elwirth <elias.wirth@bluewin.ch>
2022-08-10 11:23:45 +0200

    added stuff

--- experiments_to_latex_cgavi.py
+++ experiments_to_latex_cgavi.py
@@ -1,19 +1,20 @@
-from global_ import tos_, border_type_
+from global_ import tos_, border_type_, data_sets_performance_gb_
 from src.experiment_analysis.experiment_analysis import table_results
 
-hp_pcg = {'algorithm': 'oavi', 'oracle_type': 'PCG', 'term_ordering_strategy': tos_, 'border_type': border_type_,
+hp_pcg = {'algorithm': 'oavi', 'oracle_type': 'PCG', 'term_ordering_strategy': tos_, 'border_type': "gb",
           'inverse_hessian_boost': 'false'}
-hp_agd = {'algorithm': 'oavi', 'oracle_type': 'AGD', 'term_ordering_strategy': tos_, 'border_type': border_type_,
+hp_agd = {'algorithm': 'oavi', 'oracle_type': 'AGD', 'term_ordering_strategy': tos_, 'border_type': "gb",
           'inverse_hessian_boost': 'false'}
-hp_abm = {'algorithm': 'oavi', 'oracle_type': 'ABM', 'term_ordering_strategy': tos_, 'border_type': border_type_,
+hp_abm = {'algorithm': 'oavi', 'oracle_type': 'ABM', 'term_ordering_strategy': tos_, 'border_type': "gb",
           'inverse_hessian_boost': 'false'}
-hp_avi = {'algorithm': 'avi', 'term_ordering_strategy': tos_, 'border_type': border_type_, }
+hp_avi = {'algorithm': 'avi', 'term_ordering_strategy': tos_, 'border_type': "gb", }
 hp_vca = {'algorithm': 'vca'}
 hp_svm = {'algorithm': 'svm'}
-hps_no_svm = [hp_pcg, hp_agd, hp_abm, hp_avi, hp_vca]
+hp_bpcg = {'algorithm': 'oavi', 'oracle_type': 'BPCG', 'term_ordering_strategy': tos_, 'border_type': "gb",
+          'inverse_hessian_boost': 'false'}
+hps_no_svm = [hp_pcg, hp_agd, hp_abm, hp_vca]
 hps = hps_no_svm + [hp_svm]
-data_sets = ['bank']
-
+# data_sets_performance_gb_ = ['bank', 'credit', 'htru']
 print("############################################################################################")
 print("# Performance")
 print("############################################################################################")
@@ -23,7 +24,7 @@ for key in ["time_train", "time_test", "time_hyper", "error_train", "error_test"
     print("-------------------------------------------------------------------------")
     print("key: ", key)
     if key in ['avg_degree', "G + O", "total_sparsity"]:
-        table_results(data_sets, hps_no_svm, key, ordering=False)
+        table_results(data_sets_performance_gb_, hps_no_svm, key, ordering=False)
     else:
-        table_results(data_sets, hps, key, ordering=False)
+        table_results(data_sets_performance_gb_, hps, key, ordering=False)
 print("############################################################################################")
